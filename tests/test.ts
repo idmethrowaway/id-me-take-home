03e26f642de8eeead8fc1f1b9c10fb306bf83e2d
Thinh Vu <thinh@bitski.com>
2023-06-12 19:17:11 -0400

    Add test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,16 @@
 import { expect, test } from '@playwright/test';
+import data from '../src/routes/data.json' assert { type: 'json' };
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Purchases' })).toBeVisible();
+});
+
+test('index page has expected purchases listed', async ({ page }) => {
+	await page.goto('/');
+	await Promise.all(
+		data.map(async (purchase) => {
+			await expect(page.getByText(purchase.name).first()).toBeAttached();
+		})
+	);
 });
